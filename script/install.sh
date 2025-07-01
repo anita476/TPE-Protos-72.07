3729ab3df260785fb06f3988f6df04631749e0fc
juane993 <jolivamorroni@itba.edu.ar>
2025-07-01 20:32:33 -0300

    added libncurses-dev to install

--- script/install.sh
+++ script/install.sh
@@ -5,3 +5,4 @@ apt install check --yes # to run tests
 apt install ncat --yes # to connect client
 apt install iproute2 --yes # to check if server is running
 apt install dialog --yes # to run admin interface
+apt-get install libncurses-dev
