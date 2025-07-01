55aef21a619fb56f63d61d23dcb3d65cfa545201
juane993 <jolivamorroni@itba.edu.ar>
2025-07-01 19:51:44 -0300

    basic communicastion flow between binary code and dialog

--- script/adminInterface.sh
+++ script/adminInterface.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
-USR=$(dialog --title "Username" --inputbox "Enter your username:" 0 0 3>&1 1>&2 2>&3 3>&-); clear
-PASS=$(dialog --title "Password" --clear --insecure --passwordbox "Enter your password:" 0 0 3>&1 1>&2 2>&3 3>&-); clear
+#just for testing compile the client main.c file
+gcc ../src/client/main.c -o client.out
 
-if [[ "$USR" != "nep" || "$PASS" != "nep" ]]; then
-    dialog --title "Error" --msgbox "Username or password cannot be empty." 0 0
-    exit 1
-fi
-echo $USR
-echo $PASS
-OPTION=$(dialog --title "Admin Interface" --menu "Choose an option:" 0 0 0 \
-    1 "View System Status" \
-    2 "Manage Users" \
-    3 "Configure Settings" \
-    4 "Exit" 3>&1 1>&2 2>&3);
-clear
-echo $OPTION
+coproc CLIENT { ./client.out; }
+
+# CLIENT[1] is the app’s stdin; CLIENT[0] is its stdout
+# e.g. write "foo" to it with:   echo foo >&"${CLIENT[1]}"
+#       read from it with:        read -r line <&"${CLIENT[0]}"
+
+USR=$(dialog --title "Username" --inputbox "Enter your username:" 0 0 3>&1 1>&2 2>&3 3>&-); clear
+echo "$USR" >&"${CLIENT[1]}"
+PASS=$(dialog --title "Password" --clear --insecure --passwordbox "Enter your password:" 0 0 3>&1 1>&2 2>&3 3>&-); clear
+echo "$PASS" >&"${CLIENT[1]}"
+
+read -r line <&"${CLIENT[0]}"
+echo "$line"
+
+
+#if [[ "$USR" != "nep" || "$PASS" != "nep" ]]; then
+#    dialog --title "Error" --msgbox "Username or password cannot be empty." 0 0
+#    exit 1
+#fi
+#echo $USR
+#echo $PASS
+#OPTION=$(dialog --title "Admin Interface" --menu "Choose an option:" 0 0 0 \
+#    1 "View System Status" \
+#    2 "Manage Users" \
+#    3 "Configure Settings" \
+#    4 "Exit" 3>&1 1>&2 2>&3);
+#clear
+#echo $OPTION
--- src/client/main.c
+++ src/client/main.c
@@ -1,8 +1,64 @@
 
+
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+uint8_t checkUser(char * username, char * password) {
+	//return strcmp("nep",username) == 0 && strcmp("123", password) == 0;
+	int validU = strcmp("nep",username);
+	int validP = strcmp("123", password);
+	return validP==0 && validU==0;
+}
 int main(void) {
 	// Client
 
-	printf("Im a client\n");
+	char raw_buffer_read[256] = {0};
+	char raw_buffer_write[256] = {0};
+
+	char * raw_buffer_read_ptr = raw_buffer_read;
+	char * raw_buffer_write_ptr = raw_buffer_write;
+
+	// buffer * buff_read;
+	// buffer_init(buff_read,sizeof(raw_buffer_read), raw_buffer_read );
+	// buffer * buff_write;
+	// buffer_init(buff_write, sizeof(raw_buffer_write), raw_buffer_write);
+	//
+	// uint8_t * rb = buffer_read_ptr(buff_read,10);
+
+	uint8_t nread = read(STDIN_FILENO, raw_buffer_read_ptr, 20);
+	if (nread <= 0) {
+		perror("Read error");
+		return 1;
+	}
+	char username[20] = {0};
+	char password[20] = {0};
+	strncpy(username, raw_buffer_read_ptr, nread - 1);
+
+	nread = read(STDIN_FILENO, raw_buffer_read_ptr, 20);
+	if (nread <= 0) {
+		perror("Read error");
+		return 1;
+	}
+	strncpy(password, raw_buffer_read_ptr, nread - 1);
+
+
+	uint8_t is_usr_valid = checkUser(username, password);
+	if (is_usr_valid == 0) {
+		printf("Invalid User!!!\n");
+		return 1;
+	}
+	printf("valid User!!!\n");
+
+	// while (1) {
+	// 	uint8_t nread = read(STDIN_FILENO, raw_buffer_read_ptr, 256);
+	// 	if (nread <= 0) {
+	// 		perror("Read error");
+	// 		break;
+	// 	}
+	// 	raw_buffer_read_ptr += nread;
+	//
+	// }
 	return 0;
 }
