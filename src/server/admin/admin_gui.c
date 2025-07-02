b6a172cce08eecce1184ba3d2769a2a1ac76170f
Matías Leporini <matilepo03@gmail.com>
2025-07-02 17:08:04 -0300

    More user options in GUI

--- src/server/admin/admin_gui.c
+++ src/server/admin/admin_gui.c
@@ -21,6 +21,9 @@ void configure_settings(void);
 void admin_menu(void);
 int confirm_exit(void);
 int authenticate(void);
+int add_user(void);
+int remove_user(void);
+int change_user_password(void);
 
 int execute_dialog(const char* dialog_cmd, char* result, size_t result_size) {
     FILE* fp;
@@ -65,7 +68,7 @@ void show_info(const char* message) {
 int get_username(char* username, size_t size) {
     char cmd[MAX_COMMAND];
     snprintf(cmd, sizeof(cmd), 
-        "dialog --title \"Username\" --inputbox \"\\nEnter your username:\" 9 40 3>&1 1>&2 2>&3 3>&-");
+        "dialog --title \"Username\" --inputbox \"\\nEnter username:\" 9 40 3>&1 1>&2 2>&3 3>&-");
     
     return execute_dialog(cmd, username, size);
 }
@@ -73,7 +76,7 @@ int get_username(char* username, size_t size) {
 int get_password(char* password, size_t size) {
     char cmd[MAX_COMMAND];
     snprintf(cmd, sizeof(cmd), 
-        "dialog --title \"Password\" --clear --insecure --passwordbox \"\\nEnter your password:\" 9 40 3>&1 1>&2 2>&3 3>&-");
+        "dialog --title \"Password\" --clear --insecure --passwordbox \"\\nEnter password:\" 9 40 3>&1 1>&2 2>&3 3>&-");
     
     return execute_dialog(cmd, password, size);
 }
@@ -137,13 +140,13 @@ void manage_users() {
                 break;
             }
             case 2:
-                show_info("Add user functionality not implemented yet");
+                add_user();
                 break;
             case 3:
-                show_info("Remove user functionality not implemented yet");
+                remove_user();
                 break;
             case 4:
-                show_info("Change password functionality not implemented yet");
+                change_user_password();
                 break;
             case 5:
                 return;
@@ -296,6 +299,220 @@ int authenticate() {
     
     show_error("Maximum number of attempts reached. Access denied.");
     return 0;
+}
+
+int add_user() {
+    char username[MAX_INPUT];
+    char password[MAX_INPUT];
+    char confirm_password[MAX_INPUT];
+    
+    if (get_username(username, sizeof(username)) != 0) {
+        return 0;
+    }
+    
+    if (strlen(username) == 0) {
+        show_error("Username cannot be empty");
+        return 0;
+    }
+    
+    if (strlen(username) < 3) {
+        show_error("Username must be at least 3 characters long");
+        return 0;
+    }
+
+    if (strlen(username) >= 24) {
+        show_error("Username must be less than 24 characters long");
+        return 0;
+    }
+    
+    // TODO: Check if username already exists
+    if (strcmp(username, "nep") == 0 || strcmp(username, "user1") == 0 || 
+        strcmp(username, "user2") == 0 || strcmp(username, "guest") == 0) {
+        show_error("Username already exists. Please choose a different username.");
+        return 0;
+    }
+    
+    if (get_password(password, sizeof(password)) != 0) {
+        return 0;
+    }
+    
+    if (strlen(password) == 0) {
+        show_error("Password cannot be empty");
+        return 0;
+    }
+    
+    if (strlen(password) < 4) {
+        show_error("Password must be at least 4 characters long");
+        return 0;
+    }
+
+    if (strlen(password) >= 24) {
+        show_error("Password must be less than 24 characters long");
+        return 0;
+    }
+    
+    char cmd[MAX_COMMAND];
+    snprintf(cmd, sizeof(cmd), 
+        "dialog --title \"Confirm password\" --clear --insecure --passwordbox \"\\nConfirm your password:\" 9 40 3>&1 1>&2 2>&3 3>&-");
+    
+    if (execute_dialog(cmd, confirm_password, sizeof(confirm_password)) != 0) {
+        return 0;
+    }
+    
+    if (strcmp(password, confirm_password) != 0) {
+        show_error("Passwords do not match. Please try again.");
+        return 0;
+    }
+    
+    // TODO: Add user
+    char success_msg[512];
+    snprintf(success_msg, sizeof(success_msg), 
+        "User '%.20s' has been successfully added to the system.", username);
+    show_info(success_msg);
+    
+    memset(password, 0, sizeof(password));
+    memset(confirm_password, 0, sizeof(confirm_password));
+    
+    return 1;
+}
+
+int remove_user() {
+    char cmd[MAX_COMMAND];
+    char choice[16];
+    
+    // List of users to remove (excluding admin)
+    snprintf(cmd, sizeof(cmd),
+        "dialog --title \"Remove user\" --menu \"\\nSelect user to remove:\" 14 50 4 "
+        "1 \"user1\" "
+        "2 \"user2\" "
+        "3 \"guest\" "
+        "4 \"Cancel\" 3>&1 1>&2 2>&3");
+    
+    if (execute_dialog(cmd, choice, sizeof(choice)) != 0 || atoi(choice) == 4) {
+        return 0;
+    }
+    
+    char selected_user[32];
+    switch (atoi(choice)) {
+        case 1:
+            strcpy(selected_user, "user1");
+            break;
+        case 2:
+            strcpy(selected_user, "user2");
+            break;
+        case 3:
+            strcpy(selected_user, "guest");
+            break;
+        default:
+            show_error("Invalid selection");
+            return 0;
+    }
+    
+    char confirm_msg[256];
+    snprintf(confirm_msg, sizeof(confirm_msg),
+        "\\nAre you sure you want to remove user '%s'?\\n\\nThis action cannot be undone.", selected_user);
+    
+    snprintf(cmd, sizeof(cmd),
+        "dialog --title \"Confirm removal\" --yesno \"%s\" 10 50", confirm_msg);
+
+    // TODO: Actually remove user from system
+    if (system(cmd) == 0) {
+        char success_msg[256];
+        snprintf(success_msg, sizeof(success_msg), 
+            "User '%.20s' has been successfully removed from the system.", selected_user);
+        show_info(success_msg);
+        return 1;
+    }
+    
+    show_info("User removal cancelled.");
+    return 0;
+}
+
+int change_user_password() {
+    char cmd[MAX_COMMAND];
+    char choice[16];
+    
+    snprintf(cmd, sizeof(cmd),
+        "dialog --title \"Change password\" --menu \"\\nSelect user to change password:\" 14 50 5 "
+        "1 \"nep (Administrator)\" "
+        "2 \"user1\" "
+        "3 \"user2\" "
+        "4 \"guest\" "
+        "5 \"Cancel\" 3>&1 1>&2 2>&3");
+    
+    if (execute_dialog(cmd, choice, sizeof(choice)) != 0 || atoi(choice) == 5) {
+        return 0;
+    }
+    
+    char selected_user[32];
+    switch (atoi(choice)) {
+        case 1:
+            strcpy(selected_user, "nep");
+            break;
+        case 2:
+            strcpy(selected_user, "user1");
+            break;
+        case 3:
+            strcpy(selected_user, "user2");
+            break;
+        case 4:
+            strcpy(selected_user, "guest");
+            break;
+        default:
+            show_error("Invalid selection");
+            return 0;
+    }
+    
+    char new_password[MAX_INPUT];
+    char confirm_password[MAX_INPUT];
+    
+    snprintf(cmd, sizeof(cmd), 
+        "dialog --title \"New password\" --clear --insecure --passwordbox \"\\nEnter new password for user '%s':\" 10 50 3>&1 1>&2 2>&3 3>&-", selected_user);
+    
+    if (execute_dialog(cmd, new_password, sizeof(new_password)) != 0) {
+        return 0;
+    }
+    
+    if (strlen(new_password) == 0) {
+        show_error("Password cannot be empty");
+        return 0;
+    }
+    
+    if (strlen(new_password) < 4) {
+        show_error("Password must be at least 4 characters long");
+        return 0;
+    }
+
+    if (strlen(new_password) >= 24) {
+        show_error("Password must be less than 24 characters long");
+        return 0;
+    }
+    
+    snprintf(cmd, sizeof(cmd), 
+        "dialog --title \"Confirm password\" --clear --insecure --passwordbox \"\\nConfirm new password:\" 9 40 3>&1 1>&2 2>&3 3>&-");
+    
+    if (execute_dialog(cmd, confirm_password, sizeof(confirm_password)) != 0) {
+        memset(new_password, 0, sizeof(new_password));
+        return 0;
+    }
+    
+    if (strcmp(new_password, confirm_password) != 0) {
+        show_error("Passwords do not match. Please try again.");
+        memset(new_password, 0, sizeof(new_password));
+        memset(confirm_password, 0, sizeof(confirm_password));
+        return 0;
+    }
+    
+    // TODO: Actually change user password in system
+    char success_msg[256];
+    snprintf(success_msg, sizeof(success_msg), 
+        "Password for user '%.20s' has been successfully changed.", selected_user);
+    show_info(success_msg);
+    
+    memset(new_password, 0, sizeof(new_password));
+    memset(confirm_password, 0, sizeof(confirm_password));
+    
+    return 1;
 }
 
 int main() {
