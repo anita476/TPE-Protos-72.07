5223639498faf07a1ce56e20cf15b221c06ae1f8
Ana Negre <98236062+anita476@users.noreply.github.com>
2025-07-04 11:19:01 -0300

    changing buffer function

--- src/server/management.c
+++ src/server/management.c
@@ -1,13 +1,24 @@
 #include "include/management.h"
+#include "include/config.h"
 #include "include/logger.h"
 
 /*************************************Proper action*******************************************************/
 /**************Fetch the info or change the config itself (called by the protocol handlers)***************/
-static void handle_config_command(COMMAND cmd);
+static void handle_config_command(COMMAND cmd, const char *arg, const int argc);
 
-static void handle_config_command(COMMAND cmd) {
+// char * is the easiest since the user writes to stdin
+static void handle_config_command(COMMAND cmd, const char *arg, const int argc) {
 	switch (cmd) {
 		case CMD_BUFFER_SOCKS5:
+			if (argc != 1) {
+				log(ERROR, "CMD_BUFFER_SOCKS5 requires one argument, got %d", argc);
+				return;
+			}
+			size_t new_size = strtoul(arg, NULL, 10);
+			if (new_size > 0) { // TODO maybe have a min and max to protect the server integrity :p
+				log(INFO, "Changing SOCKS5 buffer size to %zu bytes", new_size);
+				g_socks5_buffer_size = new_size;
+			}
 			break;
 		case CMD_TIMEOUT:
 			break;
